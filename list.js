25d2ac8abf83711671f224d6c8d2a77951c2e104
Luis Quintanilla <equalsgains@gmail.com>
2016-12-10 12:34:52 -0700

    finished

--- list.js
+++ list.js
@@ -5,11 +5,15 @@ var input = prompt("What would you like to do?");
 while (input !== "quit") {
 
   if(input === "list") {
+    //handle input
     console.log(todos);
   } else if(input === "new") {
+    //ask for new todo
     var newTodo = prompt("Enter new todo");
+    //add todos array
      todos.push(newTodo);
   }
+  //ask again for ndw input
   input = prompt("What would you like to do?");
 }
 console.log("ok, you quit the app");
