2fc9b32597349021a4c621fc53b1f6e79ff1a55d
Luis Quintanilla <equalsgains@gmail.com>
2016-12-14 02:28:28 -0700

    cleaned the code by creating functions, and delcared them in the while loop

--- list.js
+++ list.js
@@ -2,29 +2,40 @@ var todos = ["Buy new turtle"];
 
 var input = prompt("What would you like to do?");
 
+function listTodos() {
+  console.log("********************");
+  todos.forEach(function(todo, i){
+  console.log(i + ": " + todo);
+  })
+  console.log("********************");
+
+}
+
+function addTodo() {
+  //ask for new todo
+  var newTodo = prompt("Enter new todo");
+  //add todos array
+  todos.push(newTodo);
+  console.log("Added " + newTodo + " to the list");
+}
+
+function deleteTodo() {
+  //ask for index of todo to be delted
+  var index = prompt("Enter index of todo to delete");
+  //delete that todo
+  //1 represents how many items in the array should be delted
+  todos.splice(index,1);
+  console.log("delted to do");
+}
 while (input !== "quit") {
 
   //handle input
   if(input === "list") {
-      console.log("********************");
-    todos.forEach(function(todo, i){
-      console.log(i + ": " + todo);
-    })
-    console.log("********************");
-
+    listTodos();
   } else if(input === "new") {
-    //ask for new todo
-    var newTodo = prompt("Enter new todo");
-    //add todos array
-     todos.push(newTodo);
-     console.log("Added " + newTodo + " to the list");
+    addTodo();
   } else if (input === "delete") {
-    //ask for index of todo to be delted
-    var index = prompt("Enter index of todo to delete");
-    //delete that todo
-    //1 represents how many items in the array should be delted
-    todos.splice(index,1);
-    console.log("delted to do");
+    deleteTodo();
   }
   //ask again for ndw input
   input = prompt("What would you like to do?");
