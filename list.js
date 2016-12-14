176d5a366b9f8f536d025ed9ddb6d3663f817f61
Luis Quintanilla <equalsgains@gmail.com>
2016-12-14 02:21:16 -0700

    Added Deleted feature

--- list.js
+++ list.js
@@ -6,15 +6,25 @@ while (input !== "quit") {
 
   //handle input
   if(input === "list") {
-    todos.forEach(function(todo){
-      console.log(todo);
+      console.log("********************");
+    todos.forEach(function(todo, i){
+      console.log(i + ": " + todo);
     })
-    // console.log(todos);
+    console.log("********************");
+
   } else if(input === "new") {
     //ask for new todo
     var newTodo = prompt("Enter new todo");
     //add todos array
      todos.push(newTodo);
+     console.log("Added " + newTodo + " to the list");
+  } else if (input === "delete") {
+    //ask for index of todo to be delted
+    var index = prompt("Enter index of todo to delete");
+    //delete that todo
+    //1 represents how many items in the array should be delted
+    todos.splice(index,1);
+    console.log("delted to do");
   }
   //ask again for ndw input
   input = prompt("What would you like to do?");
