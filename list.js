fac6cb566ab123a5efe3fc02aec4644cb14a9879
Luis Quintanilla <equalsgains@gmail.com>
2016-12-14 01:44:14 -0700

    cleaning up the code

--- list.js
+++ list.js
@@ -4,9 +4,12 @@ var input = prompt("What would you like to do?");
 
 while (input !== "quit") {
 
+  //handle input
   if(input === "list") {
-    //handle input
-    console.log(todos);
+    todos.forEach(function(todo){
+      console.log(todo);
+    })
+    // console.log(todos);
   } else if(input === "new") {
     //ask for new todo
     var newTodo = prompt("Enter new todo");
